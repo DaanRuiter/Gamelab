fc43305fdcc4057a10ecd538fba3cdc536794114
menno <run_jongejan@hotmail.com>
2015-04-23 11:56:55 +0200

    sync

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,15 +1,62 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour {
+	[SerializeField]private GameObject _playButton;
+	[SerializeField]private GameObject _creditsButton;
+	[SerializeField]private GameObject _optionsButton;
 
-	// Use this for initialization
-	void Start () {
-	
+	[SerializeField]private GameObject _creditsCanvas;
+	[SerializeField]private GameObject _optionsCanvas;
+	public void PlayGame()
+	{
+		StartCoroutine(MoveButton(_playButton));
+		StartCoroutine(FadeButton(_creditsButton));
+		StartCoroutine(FadeButton(_optionsButton));
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public void OpenCredits()
+	{
+
+	}
+	public void OpenOptions()
+	{
+
+	}
+	public void BackToMenu()
+	{
+
+	}
+	private IEnumerator MoveButton(GameObject button)
+	{
+		bool inScreen = true;
+		RectTransform buttonRectTransform = button.GetComponent<RectTransform>();
+		Vector3 newPos = buttonRectTransform.position;
+		while(inScreen)
+		{
+			newPos.z += 0.1f;
+			buttonRectTransform.position = newPos;
+			if(newPos.z >= 5)
+			{
+				inScreen = false;
+			}
+			yield return new WaitForEndOfFrame();
+		}
+	}
+	private IEnumerator FadeButton(GameObject button)
+	{
+		Image buttonImage = button.GetComponent<Image>();
+		Color newColor = buttonImage.color;
+		bool fading = true;
+		while(fading)
+		{
+			newColor.a -= 0.01f;
+			buttonImage.color = newColor;
+			if(newColor.a <= 0)
+			{
+				fading = false;
+			}
+			yield return new WaitForEndOfFrame();
+		}
 	}
 }
